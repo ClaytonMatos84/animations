5634fa5a7f8446ec5edb5def0392f7ce94aa8771
ClaytonMatos1 <clayton-matos1@hotmail.com>
2022-01-19 19:42:05 -0300

    [master] inclusao da logica de animacao com o scroll

---
+++ index.js
@@ -0,0 +1,26 @@
+function init() {
+    const moveElement1 = document.getElementById('moveConteudo1');
+    const moveElement2 = document.getElementById('moveConteudo2');
+    const moveElement3 = document.getElementById('moveConteudo3');
+    const moveElement4 = document.getElementById('moveConteudo4');
+    const arrayElements = [moveElement1, moveElement2, moveElement3, moveElement4];
+
+    function moveContent(element, index) {
+        const property = (index % 2 == 0) ? 'marginLeft' : 'marginRight';
+        if (element.getBoundingClientRect().top > 0 &&
+            ((element.getBoundingClientRect().top + 70) <= window.innerHeight)) {
+            element.style[property] = '0';
+        } else if (element.getBoundingClientRect().bottom > 0 &&
+            ((element.getBoundingClientRect().bottom - (element.getBoundingClientRect().height + 140)) + element.getBoundingClientRect().height < 0)) {
+            element.style[property] = '-800px';
+        }
+    }
+
+    window.addEventListener('scroll', () => {
+        arrayElements.forEach((element, index) => {
+            moveContent(element, index);
+        });
+    });
+}
+
+window.addEventListener('load', this.init);
