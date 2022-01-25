39154433085b667686663acca1e2508b10e30df2
cmatos <cmatos@uolinc.com>
2022-01-25 19:56:41 -0300

    [master] animacao scroll opacity

--- index.js
+++ index.js
@@ -14,15 +14,41 @@ function init() {
         } else if (element.style[property] != '-800px' &&
             element.getBoundingClientRect().bottom > 0 &&
             ((element.getBoundingClientRect().bottom - (element.getBoundingClientRect().height + 140)) + element.getBoundingClientRect().height < 0 ||
-            element.getBoundingClientRect().bottom >= (window.innerHeight + element.getBoundingClientRect().height - 80))) {
+                element.getBoundingClientRect().bottom >= (window.innerHeight + element.getBoundingClientRect().height - 80))) {
             element.style[property] = '-800px';
         }
     }
+
+    const imgHelper1 = document.getElementById('imgHelper1');
+    const imgHelper2 = document.getElementById('imgHelper2');
+    const imgHelper3 = document.getElementById('imgHelper3');
+    const imgHelper4 = document.getElementById('imgHelper4');
+
+    function hiddenHelperImg() {
+        if ((imgHelper1.style.opacity != 0 || imgHelper1.style.opacity == '') &&
+            (imgHelper1.getBoundingClientRect().top <= 350 && imgHelper1.getBoundingClientRect().top > 20)) {
+            imgHelper1.style.opacity = '0';
+            imgHelper2.style.opacity = '1';
+
+            imgHelper3.style.opacity = '0';
+            imgHelper4.style.opacity = '1';
+        } else if ((imgHelper1.style.opacity != 1 || imgHelper1.style.opacity == '') &&
+            (imgHelper1.getBoundingClientRect().top > 350 || imgHelper1.getBoundingClientRect().top <= 20)) {
+            imgHelper1.style.opacity = '1';
+            imgHelper2.style.opacity = '0';
+
+            imgHelper3.style.opacity = '1';
+            imgHelper4.style.opacity = '0';
+        }
+    }
+
 
     window.addEventListener('scroll', () => {
         arrayElements.forEach((element, index) => {
             moveContent(element, index);
         });
+
+        hiddenHelperImg();
     });
 
     const actualYear = document.getElementById('anoAtual');
